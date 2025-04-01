40d3a55072995fec0319c165ac496397f8cecb53
innocypapatop <moonwalahehe@gmail.com>
2025-04-02 02:24:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -5,4 +5,4 @@ class Config(object):
     # Pyrogram Client
     TOKEN = os.environ.get("TOKEN", "8190350217:AAFPBQoEK96Dkg7ILHM9louqXLt6c8SOJUs") # ⚠️ Required
     # Other Configs
-    SUDO = list(map(int, os.environ.get("SUDO", "7899610698 7538572906").split(",")))  # ⚠️ Required
+    SUDO = list(map(int, os.environ.get("SUDO", "7899610698,7538572906").split(",")))  # ⚠️ Required
